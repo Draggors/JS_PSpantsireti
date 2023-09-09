6b9d65d9d72811086f513a98efc40a48136d0726
Draggors <pavlospantsireti@gmail.com>
2023-09-09 23:40:37 +0300

    HomeWork 01

---
+++ Tests/register_test.js
@@ -0,0 +1,23 @@
+Feature('register');
+
+Scenario('test something', ({ I }) => {
+I.amOnPage('http://opencart.qatestlab.net/index.php');
+alert('');
+
+I.click({ xpath: '//*[@id="top-links"]/ul/li/span/span' });
+I.click({ xpath: '//*[@id="top-links"]/ul/li/ul/li[1]/a' });
+
+I.fillField({ xpath: '//*[@id="input-firstname"]'}, "Joseph");
+I.fillField({ xpath: '//*[@id="input-lastname"]'}, "Ferretti");
+I.fillField({ xpath: '//*[@id="input-email"]'}, "josephFerretti@gmail.com");
+I.fillField({ xpath: '//*[@id="input-telephone"]'}, "+380999999999");
+I.fillField({ xpath: '//*[@id="input-password"]'}, "J0sEph12345");
+I.fillField({ xpath: '//*[@id="input-confirm"]'}, "J0sEph12345");
+
+I.click({xpath: '//*[@id="content"]/form/div/div/input[1]'});
+I.click({xpath: '//*[@id="content"]/form/div/div/input[2]'});
+
+const regTitleText = 'Your Account Has Been Created!'
+I.seeTextEquals(regTitleText, {xpath: '//*[@id="content"]/h1'})
+});
+
---
+++ codecept.conf.js
@@ -0,0 +1,28 @@
+const { setHeadlessWhen, setCommonPlugins } = require('@codeceptjs/configure');
+// turn on headless mode when running with HEADLESS=true environment variable
+// export HEADLESS=true && npx codeceptjs run
+setHeadlessWhen(process.env.HEADLESS);
+
+// enable all common plugins https://github.com/codeceptjs/configure#setcommonplugins
+setCommonPlugins();
+
+/** @type {CodeceptJS.MainConfig} */
+exports.config = {
+  tests: './Tests/*_test.js',
+  output: './output',
+  helpers: {
+    Playwright: {
+      browser: 'chromium',
+      url: 'http://localhost',
+      show: true,
+      waitForNavigation: "networkidle",
+      waitForTimeout: 5000,
+      windowSize: '1920x1080',
+
+    }
+  },
+  include: {
+    I: './steps_file.js'
+  },
+  name: 'JS_PSpantsireti'
+}
