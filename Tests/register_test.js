72bffd5556079a3e42c5f29e3e66437ed82f02bc
Draggors <pavlospantsireti@gmail.com>
2023-09-10 00:44:47 +0300

    d1

--- Tests/register_test.js
+++ Tests/register_test.js
@@ -2,13 +2,12 @@ Feature('register');
 
 Scenario('test something', ({ I }) => {
 I.amOnPage('http://opencart.qatestlab.net/index.php');
-alert('');
 
 I.click({ xpath: '//*[@id="top-links"]/ul/li/span/span' });
 I.click({ xpath: '//*[@id="top-links"]/ul/li/ul/li[1]/a' });
 
 I.fillField({ xpath: '//*[@id="input-firstname"]'}, "Joseph");
-I.fillField({ xpath: '//*[@id="input-lastname"]'}, "Ferretti");
+I.fillField({ xpath: '//*[@id="input-lastname"]'}, "12345");
 I.fillField({ xpath: '//*[@id="input-email"]'}, "josephFerretti@gmail.com");
 I.fillField({ xpath: '//*[@id="input-telephone"]'}, "+380999999999");
 I.fillField({ xpath: '//*[@id="input-password"]'}, "J0sEph12345");
