0eb32ee9b97bb4633dfb0d6397d1ea6ca49c3b2a
Draggors <pavlospantsireti@gmail.com>
2023-09-10 22:43:38 +0300

    1

--- pages/product.js
+++ pages/product.js
@@ -6,21 +6,20 @@ module.exports = {
   sizeDropDown: { xpath: '//label[text()="Size"]/following-sibling::div/a[1]' },
   colorOption: { xpath: '//label[text()="Color"]/following-sibling::div/ul/li[2]' },
   sizeOption: { xpath: '//label[text()="Size"]/following-sibling::div/ul/li[2]' },
-  priceText: { xpath: '/div/span[@class="price-new"]'},
+  priceText: { xpath: '/div/span[@class="price-new"]' },
 
   selectColor() {
     I.click(this.colorDropDown);
     I.click(this.colorOption);
   },
-
+  selectSize() {
+    I.click(this.sizeDropDown);
+    I.click(this.sizeOption);
+  },
   async getProductPrice() {
     const draftProductPrice = await I.grabTextForm(this.priceText)
     const colorPrice = await I.grabTextFrom(this.colorOption);
     const sizePrice = await I.grabTextFrom(this.sizeOption);
-    return  colorPrice.slice(-6,-1);
-  },
-  selectSize() {
-    I.click(this.SizeDropDown);
-    I.click(this.SizeOption);
+    return colorPrice.slice(-6, -1);
   },
 }
