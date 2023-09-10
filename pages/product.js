879908c2b4a8ac8246c2040f19934115e444115b
Draggors <pavlospantsireti@gmail.com>
2023-09-10 22:33:47 +0300

    1

--- pages/product.js
+++ pages/product.js
@@ -2,14 +2,22 @@ const { I } = inject();
 
 module.exports = {
 
-  ColorDropDown: { xpath: '//label[text() = "Color"]/following-sibling::div/a[1]' },
-  SizeDropDown: { xpath: '//label[text()="Size"]/following-sibling::div/a[1]' },
-  ColorOption: { xpath: '//label[text()="Color"]/following-sibling::div/ul/li[2]'},
-  SizeOption: { xpath: '//label[text()="Size"]/following-sibling::div/ul/li[2]'},
+  colorDropDown: { xpath: '//label[text() = "Color"]/following-sibling::div/a[1]' },
+  sizeDropDown: { xpath: '//label[text()="Size"]/following-sibling::div/a[1]' },
+  colorOption: { xpath: '//label[text()="Color"]/following-sibling::div/ul/li[2]' },
+  sizeOption: { xpath: '//label[text()="Size"]/following-sibling::div/ul/li[2]' },
+  priceText: { xpath: '/div/span[@class="price-new"]'},
 
   selectColor() {
     I.click(this.ColorDropDown);
     I.click(this.ColorOption);
+  },
+
+  async getProductPrice() {
+    const draftProductPrice = await I.grabTextForm(this.priceText)
+    const colorPrice = await I.grabTextFrom(this.colorOption);
+    const sizePrice = await I.grabTextFrom(this.sizeOption);
+    return  colorPrice.slice(-6,-1);
   },
   selectSize() {
     I.click(this.SizeDropDown);
