2275725272e8303f0dd542fa65e24dfdc0f50b9c
Draggors <pavlospantsireti@gmail.com>
2023-09-10 15:22:08 +0300

    23322

--- steps.d.ts
+++ steps.d.ts
@@ -4,9 +4,10 @@ type basePage = typeof import('./pages/BasePage.js');
 type accountPage = typeof import('./pages/account.js');
 type confirmRegPage = typeof import('./pages/confirmReg.js');
 type confirmPage = typeof import('./pages/confirm.js');
+type finalPage = typeof import('./pages/final.js');
 
 declare namespace CodeceptJS {
-  interface SupportObject { I: I, current: any, basePage: basePage, accountPage: accountPage, confirmRegPage: confirmRegPage, confirmPage: confirmPage }
+  interface SupportObject { I: I, current: any, basePage: basePage, accountPage: accountPage, confirmRegPage: confirmRegPage, confirmPage: confirmPage, finalPage: finalPage }
   interface Methods extends Playwright {}
   interface I extends ReturnType<steps_file> {}
   namespace Translation {
