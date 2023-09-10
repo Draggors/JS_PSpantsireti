d88652165d21c848f54e60259e7ca64c5b6f1d66
Draggors <pavlospantsireti@gmail.com>
2023-09-10 20:17:20 +0300

    1

--- pages/product.js
+++ pages/product.js
@@ -4,7 +4,7 @@ module.exports = {
 
   ColorDropDown: { xpath: '//label[text() = "Color"]/following-sibling::div/a[1]' },
   SizeDropDown: { xpath: '//label[text()="Size"]/following-sibling::div/a[1]' },
-  ColorOption: { xpath: '//label[text()="Size"]/following-sibling::div/ul/li[2]'},
+  ColorOption: { xpath: '//label[text()="Color"]/following-sibling::div/ul/li[2]'},
   SizeOption: { xpath: '//label[text()="Size"]/following-sibling::div/ul/li[2]'},
 
   selectColor() {
