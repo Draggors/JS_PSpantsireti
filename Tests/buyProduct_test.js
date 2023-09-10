34ca8ea206e265e1b6de2735da828ce1c4134851
Draggors <pavlospantsireti@gmail.com>
2023-09-10 18:53:30 +0300

    1

--- Tests/buyProduct_test.js
+++ Tests/buyProduct_test.js
@@ -1,16 +1,13 @@
 const USER = {
-    firstName: 'Joseph',
-    lastName: 12345,
-    email: Date.now() + '@gmail.com',
-    telephone:"+380999999999",
-    password: "J0sEph12345",
-    confirmPassword: "J0sEph12345", 
-
-    
+    email:'josephferretti@gmail.com',
+    password: "J0sEph12345",   
 }
 
 Feature('Log In + Buy');
 
 Scenario('Log In, buy', ({ I, basePage, accountPage, finalPage}) => {
+
+    I.Login(USER);
+    pause();
 
 });
