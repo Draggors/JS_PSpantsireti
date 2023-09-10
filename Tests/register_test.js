c602f95747e67693f56df63468d9271428ba036f
Draggors <pavlospantsireti@gmail.com>
2023-09-10 22:03:00 +0300

    1

--- Tests/register_test.js
+++ Tests/register_test.js
@@ -2,26 +2,26 @@ const NEW_USER = {
     firstName: 'Joseph',
     lastName: 12345,
     email: Date.now() + '@gmail.com',
-    telephone:"+380999999999",
+    telephone: "+380999999999",
     password: "J0sEph12345",
-    confirmPassword: "J0sEph12345", 
+    confirmPassword: "J0sEph12345",
 
-    
+
 }
 
 Feature('register');
 
-Scenario('Registration', ({ I, basePage, accountPage}) => {
+Scenario('Registration', ({ I, basePage, accountPage }) => {
 
-I.amOnPage('http://opencart.qatestlab.net/index.php');
+    I.amOnPage('http://opencart.qatestlab.net/index.php');
 
-basePage.clickMyAccount();
-basePage.clickRegister();
+    basePage.clickMyAccount();
+    basePage.clickRegister();
 
-accountPage.fillNewUserForm(NEW_USER);
+    accountPage.fillNewUserForm(NEW_USER);
 
-accountPage.clickPrivacy();
-accountPage.clickContinueButton();
+    accountPage.clickPrivacy();
+    accountPage.clickContinueButton();
 
-accountPage.verifySuccessfullyReg();
+    accountPage.verifySuccessfullyReg();
 });
