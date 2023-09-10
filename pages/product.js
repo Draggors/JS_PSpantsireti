e25c8040ff4e1f44bc61faf068f963ea9cc707d6
Draggors <pavlospantsireti@gmail.com>
2023-09-10 20:01:49 +0300

    1

--- pages/product.js
+++ pages/product.js
@@ -2,5 +2,17 @@ const { I } = inject();
 
 module.exports = {
 
-  // insert your locators and methods here
+  ColorDropDown: { xpath: '//label[text() = "Color"]/following-sibling::div/a[1]' },
+  SizeDropDown: { xpath: '//label[text()="Size"]/following-sibling::div/a[1]' },
+  ColorOption: { xpath: '//label[text()="Size"]/following-sibling::div/ul/li[2]'},
+  SizeOption: { xpath: '//label[text()="Size"]/following-sibling::div/ul/li[2]'},
+
+  selectColor() {
+    I.click(ColorDropDown);
+    I.click(ColorOption);
+  },
+  selectSize() {
+    I.click(SizeDropDown);
+    I.click(SizeOption);
+  }
 }
--- steps.d.ts
+++ steps.d.ts
@@ -5,9 +5,10 @@ type accountPage = typeof import('./pages/account.js');
 type confirmRegPage = typeof import('./pages/confirmReg.js');
 type confirmPage = typeof import('./pages/confirm.js');
 type finalPage = typeof import('./pages/final.js');
+type productPage = typeof import('./pages/product.js');
 
 declare namespace CodeceptJS {
-  interface SupportObject { I: I, current: any, basePage: basePage, accountPage: accountPage, confirmRegPage: confirmRegPage, confirmPage: confirmPage, finalPage: finalPage }
+  interface SupportObject { I: I, current: any, basePage: basePage, accountPage: accountPage, confirmRegPage: confirmRegPage, confirmPage: confirmPage, finalPage: finalPage, productPage: productPage }
   interface Methods extends Playwright {}
   interface I extends ReturnType<steps_file> {}
   namespace Translation {
