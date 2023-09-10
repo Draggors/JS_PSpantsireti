3132a8eb025a0033073e3dde76e255965f7316a6
Draggors <pavlospantsireti@gmail.com>
2023-09-10 20:04:54 +0300

    1

--- Tests/buyProduct_test.js
+++ Tests/buyProduct_test.js
@@ -10,10 +10,13 @@ Scenario('Log In', ({ I }) => {
     I.Login(USER);
 });
 
-Scenario('Buy Product', ({ I }) => {
+Scenario('Buy Product', ({ I, productPage, }) => {
 
     I.Login(USER);
-    I.amOnPage('http://opencart.qatestlab.net/index.php?route=product/product&product_id=44')
+    I.amOnPage('http://opencart.qatestlab.net/index.php?route=product/product&product_id=44');
+    productPage.selectColor;
+    productPage.selectSize;
+    
     pause();
 
 });
--- pages/product.js
+++ pages/product.js
@@ -14,5 +14,5 @@ module.exports = {
   selectSize() {
     I.click(SizeDropDown);
     I.click(SizeOption);
-  }
+  },
 }
